3d803cb3fe1aaf83a06baf185553e48404501e48
jm011jh <jm011jh@gmail.com>
2022-03-28 13:57:59 +0900

    Project funciton마감

--- common/js/header.js
+++ common/js/header.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
-    const audio = new Audio('/common/audio/audio.wav');
-    const soundPlayBtn = $('#soundPlayBtn')
-    var soundPlaySW = false;
-    var audioInit = false;
-    function audioPlay(){
-        if(audioInit==false){
-            audio.play();
-            audioInit = false;
-        }
-    }
-    $(window).scroll(function(){
-        setTimeout(()=>{
-            audioPlay()
-        })
-    })
-    soundPlayBtn.click(function(){
-        console.log('hi')
-        if(!soundPlaySW){
-            $(this).addClass('on')
-            $(this).removeClass('off')
-        }else{
-            $(this).addClass('off')
-            $(this).removeClass('on')
-        }
-        soundPlaySW = !soundPlaySW;
-    })
-})
+// $(document).ready(function(){
+//     const audio = new Audio('/common/audio/audio.wav');
+//     const soundPlayBtn = $('#soundPlayBtn')
+//     var soundPlaySW = false;
+//     var audioInit = false;
+//     function audioPlay(){
+//         if(audioInit==false){
+//             audio.play();
+//             audioInit = false;
+//         }
+//     }
+//     $(window).scroll(function(){
+//         setTimeout(()=>{
+//             audioPlay()
+//         })
+//     })
+//     soundPlayBtn.click(function(){
+//         console.log('hi')
+//         if(!soundPlaySW){
+//             $(this).addClass('on')
+//             $(this).removeClass('off')
+//         }else{
+//             $(this).addClass('off')
+//             $(this).removeClass('on')
+//         }
+//         soundPlaySW = !soundPlaySW;
+//     })
+// })
--- common/js/project.js
+++ common/js/project.js
@@ -5,11 +5,16 @@ Vars
 const $menu = document.querySelector('.menu')
 const $items = document.querySelectorAll('.menu--item')
 const $bgs = document.querySelectorAll('.img--box')
+const $pagination = document.querySelectorAll('.menu--pagination-list-box');
+const $paginationBG = document.querySelectorAll('.menu--pagination-list');
 
 let menuWidth = $menu.offsetWidth
 let itemWidth = $items[0].offsetWidth
 let wrapWidth = $items.length * itemWidth
 let bgWidth = $bgs[0].offsetWidth
+let paginationWidth = $paginationBG[0].offsetWidth
+let paginationWrapWidth = $pagination.length * paginationWidth
+console.log(paginationWidth)
 
 let scrollSpeed = 0
 let oldScrollY = 0
@@ -26,6 +31,8 @@ const lerp = (v0, v1, t) => {return v0 * ( 1 - t ) + v1 * t}
 Dispose
 --------------------*/
 const dispose = (scroll) => {
+  var answer = (Math.floor(scroll)/wrapWidth)*100
+  // console.log(answer)
   gsap.set($items, {
     x: (i) => {
       return i * itemWidth + scroll
@@ -37,12 +44,25 @@ const dispose = (scroll) => {
       }
     }
   })
+  gsap.set($pagination, {
+    x: (i) => {
+      return i * paginationWidth + scroll/wrapWidth * paginationWrapWidth
+    },
+    modifiers: {
+      x: (x, target) => {
+        const s = gsap.utils.wrap(-paginationWidth - 100, paginationWrapWidth - paginationWidth - 100, parseInt(x))
+        console.log(s)
+        return `${-s}px`
+      }
+    }
+  })
   gsap.set($bgs, {
     x: (i) => {
       return i * bgWidth + scroll
     },
     modifiers: {
       x: (x, target) => {
+        
         const s = gsap.utils.wrap(-itemWidth - 100, wrapWidth - itemWidth - 100, parseInt(x))
         return `${-s/80}%`
       }
@@ -110,6 +130,7 @@ window.addEventListener('resize', () => {
   bgWidth = $bgs[0].offsetWidth
 })
 
+
 /*--------------------
 Render
 --------------------*/
@@ -119,5 +140,79 @@ const render = () => {
   dispose(y)  
 }
 render()
+
+
+/*--------------------
+letter-divide
+--------------------*/
+$(".item--tit").each(function(index,item){
+
+  var text = $(this).text();
+  var arr = text.split("");
+  var space = /\s/g;
+
+  $(this).empty();
+  for(i=0;i<arr.length;i++){
+    if(arr[i].match(space)){
+      var span = $("<br>")
+      span.appendTo($(this))
+    }else{
+      var spanLv1 = $("<span></span>")
+      var spanLv2 = $("<span>"+ arr[i] + "</span>")
+      var spanLv2Clone = $("<span>"+ arr[i] + "</span>")
+      spanLv1.addClass("tit-lv1")
+      spanLv2.addClass("tit-lv2")
+      spanLv2.addClass("tit-lv2-real")
+      spanLv2Clone.addClass("tit-lv2")
+      spanLv2Clone.addClass("tit-lv2-clone")
+      
+      spanLv2.appendTo(spanLv1)
+      spanLv2Clone.appendTo(spanLv1)
+
+      spanLv1.appendTo(this)
+    }
+  }
+
+})
+$(".item--year").each(function(index,item){
+  var text = $(this).text();
+  var pReal = $("<p>" + text + "</p>")
+  var pFake = $("<p>" + text + "</p>")
+  $(this).empty()
+  pReal.addClass('p-real')
+  pFake.addClass('p-fake')
+  pReal.appendTo(this)
+  pFake.appendTo(this)
+})
+$(".item--class").each(function(index,item){
+  var text = $(this).text();
+  var pReal = $("<p>" + text + "</p>")
+  var pFake = $("<p>" + text + "</p>")
+  $(this).empty()
+  pReal.addClass('p-real')
+  pFake.addClass('p-fake')
+  pReal.appendTo(this)
+  pFake.appendTo(this)
+})
+$(".item--num").each(function(index,item){
+  var text = $(this).text();
+  var pReal = $("<p>" + text + "</p>")
+  var pFake = $("<p>" + text + "</p>")
+  $(this).empty()
+  pReal.addClass('p-real')
+  pFake.addClass('p-fake')
+  pReal.appendTo(this)
+  pFake.appendTo(this)
+})
+/*--------------------
+menu-item function
+--------------------*/
+$(".menu--item").mouseenter(function(){
+  $(".menu--item").removeClass('is-hover')
+  $(this).addClass('is-hover')
+})
+$(".menu--item").mouseleave(function(){
+  $(this).removeClass('is-hover')
+})
 
 })
