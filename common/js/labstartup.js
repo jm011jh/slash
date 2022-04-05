f58b19be1a4775449baa23cac4736382cf0b294e
jm011jh <jm011jh@gmail.com>
2022-04-05 18:00:55 +0900

    SJM labstartup 마감

--- common/js/labstartup.js
+++ common/js/labstartup.js
@@ -2,4 +2,16 @@ $(window).ready(function(){
     var swiper = new Swiper(".s8-swiper", {
         grabCursor: true,
     });
+
+    setTimeout(() => {
+        $("#sect1").addClass('init')
+    }, 100);
+    var s1score = 100000;
+    var s1scoreRepeat = setInterval(()=>{
+        s1score = s1score + 100000;
+        $('#s1score').text(s1score)
+        if(s1score>=10000000){
+            clearInterval(s1scoreRepeat)
+        }
+    },18)
 })
