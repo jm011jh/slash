6f9ef043b4c0435325f3209778b84b2ba1eac8f5
jm011jh <jm011jh@gmail.com>
2022-04-15 18:13:52 +0900

    SJM freestyle done

---
+++ common/js/freestyle.js
@@ -0,0 +1,8 @@
+$(window).ready(function(){
+    setTimeout(()=>{
+        $("#sect1").addClass('init')
+    },100)
+    setTimeout(()=>{
+        $(".s1__title").addClass('init_2')
+    },1000)
+})
